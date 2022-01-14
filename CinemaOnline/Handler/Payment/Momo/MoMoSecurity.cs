351feb0d50b912749b2f3e02784e966157f93c94
thinhnd <thinh6699@gmail.com>
2022-01-14 15:42:53 +0700

    init projecy

---
+++ CinemaOnline/Handler/Payment/Momo/MoMoSecurity.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CinemaOnline.Handler.Payment.Momo
+{
+    internal class MoMoSecurity
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MoMoSecurity()
+        {           
+        }
+
+        public string GetHash(string partnerCode, string merchantRefId,
+            string amount, string paymentCode, string storeId, string storeName, string publicKeyXML)
+        {
+            var json = "{\"partnerCode\":\"" +
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"amount\":" +
+                amount + ",\"paymentCode\":\"" +
+                paymentCode + "\",\"storeId\":\"" +
+                storeId + "\",\"storeName\":\"" +
+                storeName + "\"}";
+            log.Debug("Raw hash: " + json);
+            var data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(4096)) 
+            {
+                try
+                {                   
+                    rsa.FromXmlString(publicKeyXML);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+
+            return result;
+        }
+
+        public string BuildQueryHash(string partnerCode, string merchantRefId,
+            string requestid, string publicKey)
+        {
+            var json = "{\"partnerCode\":\"" +
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"requestId\":\"" +
+                requestid + "\"}";
+            log.Debug("Raw hash: " + json);
+            var data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // Client ma hoa du lieu voi key by server
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+
+            return result;
+        }
+
+        public string BuildRefundHash(string partnerCode, string merchantRefId,
+            string momoTranId, long amount, string description, string publicKey)
+        {
+            var json = "{\"partnerCode\":\"" +
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"momoTransId\":\"" +
+                momoTranId + "\",\"amount\":" +
+                amount + ",\"description\":\"" +
+                description + "\"}";
+            log.Debug("Raw hash: " + json);
+            var data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+
+            return result;
+        }
+
+        public string SignSha256(string message, string key)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                string hex = BitConverter.ToString(hashmessage);
+                hex = hex.Replace("-", "").ToLower();
+                return hex;
+            }
+        }
+    }
+}
